6df04a76f4340c3694e87cb1fc65d967857e67a5
Camilo Romero <camids980@gmail.com>
2024-09-02 16:43:51 -0400

    Relacion formada entre los blog y los autores

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 // 1. Importar las utilidades de `astro:content`
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
 
 // 2. Definir un `type` y `schema` para cada colección
 const blogCollection = defineCollection({
@@ -11,12 +11,23 @@ const blogCollection = defineCollection({
       description: z.string(),
       image: image().refine((img) => img.width < 1200),
       //Relacion
-      author: z.string(),
+      author: reference("author"),
       tags: z.array(z.string()),
     }),
+});
+
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) => {
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    });
+  },
 });
 
 // 3. Exportar un único objeto `collections` para registrar tu(s) colección(es)
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
